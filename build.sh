76379a7908368fdaefbc2698bbb895e2760aba4c
Solomon <GuoYuefei@protonmail.com>
2024-04-12 11:07:01 +0800

    Update build.sh
    高版本node改变了加密方式，编译脚本增加环境变量指定加密算法

--- build.sh
+++ build.sh
@@ -2,6 +2,13 @@
 
 # 仅编译web版本
 
+##Enable legacy OpenSSL provider. NODE_OPTIONS为了应对node高版本改变了加密算法；如果是windows的powershell环境请注释linux代码打开windows代码；windows的git bash环境请忽略
+# for linux
+export NODE_OPTIONS=--openssl-legacy-provider         # for linux
+
+# for windows powershell
+# $env:NODE_OPTIONS = "--openssl-legacy-provider"
+
 mkdir -p ./build
 
 cd front
